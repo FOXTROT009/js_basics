ff9ecc63757e13e6552f2cb022631e0a4e969a85
surajkhari <95219633+FOXTROT009@users.noreply.github.com>
2023-09-27 03:26:41 +0000

    dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId =  14433
+let accountEmail = "surajkhari@gmail.com"
+var accountNumber = "123"
+accountCity = "delhi"
+console.table([accountId,accountEmail,accountNumber,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node .js not browser
+
+
+// PRIMITIVE DATATYPES
+// boolean = yes/no;
+// bigint
+// string ;
+// null = standalone value;
+// undefined;
+// symbol;
+
+//OBJECT
+console.log(typeof symbol)
+
